078dd9bcfba831e624c124c514926f1c06d435ba
Okin Grigoriy <okint34@yandex.ru>
2019-03-31 01:42:23 +0300

    Initial Commit

---
+++ DepositCalc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DepositCalc
+//
+//  Created by Grisha Okin on 31/03/2019.
+//  Copyright © 2019 Grisha Okin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
